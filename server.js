6a563b910c80d7fa55c8d9a10b918ada3ba4e75f
Sam <sam.slotemaker@ziggo.nl>
2020-04-15 17:51:25 +0200

    start

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ let data = {
   title: "datingapp"
 };
 
+exports.data = data;
 
 //routes
 app
@@ -119,24 +120,24 @@ function registerForm(req, res) {
   });
 }
 
-function registerUser(req, res, next) {
-  collectionUsers.insertOne({
-      username: req.body.username,
-      email: req.body.emailadres,
-      wachtwoord: req.body.wachtwoord,
-    },
-    done
-  );
+// function registerUser(req, res, next) {
+//   collectionUsers.insertOne({
+//       username: req.body.username,
+//       email: req.body.emailadres,
+//       wachtwoord: req.body.wachtwoord,
+//     },
+//     done
+//   );
 
-  function done(err, data) {
-    if (err) {
-      next(err);
-    } else {
-      req.session.user = req.body.username;
-      res.redirect("/add");
-    }
-  }
-}
+//   function done(err, data) {
+//     if (err) {
+//       next(err);
+//     } else {
+//       req.session.user = req.body.username;
+//       res.redirect("/add");
+//     }
+//   }
+// }
 
 function compareCredentials(req, res) {
   collectionUsers.findOne({
